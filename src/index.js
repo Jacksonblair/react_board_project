195a2b5268259c6dfbe7c3933eacf4febaaf050d
Jacksonblair <jackson.blair@live.com>
2021-01-28 18:09:18 +1100

    add server url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import './index.css'
 import App from './App'
 
-axios.defaults.baseURL = 'http://localhost:4555/api';
+axios.defaults.baseURL = 'https://jackson-blair-node-demon.herokuapp.com/';
 axios.defaults.withCredentials = true
 
 let updateUserDetailsFromCookie = () => {
